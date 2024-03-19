41bbdd1aaddec8948be1926a92fe37d19d1625a2
ChangdaePark <parkchangdae2636@gmail.com>
2024-03-19 11:03:14 +0900

    [Feat] Class 관련 소스 샘플 테스트 코딩
    생성, 소멸, 접근제어자, 스크립 터블

---
+++ My project/Assets/Scripts/ExAccessControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExAccessControl : MonoBehaviour
+{
+    public int publicValue;
+
+    private int privateValue;
+
+    protected int protectedValue;
+
+    internal int internalValue;
+
+  
+    }
